6ba3cef93e19746d2b88b78d433ee56db38c7c73
Sufyan Shaikh <absufyn@gmail.com>
2021-07-30 11:06:12 +0530

    components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GlassComponent } from './glass/glass.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: '',
+    component: GlassComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { GlassComponent } from './glass/glass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    GlassComponent
   ],
   imports: [
     BrowserModule,
